81ffc2cb53aa37057f88192dc7a21a5f5230624d
Naveen <hcnaveen8217@gmail.com>
2025-08-29 13:07:55 +0530

    config server commit

---
+++ config-server/src/test/java/com/navi/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navi.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
